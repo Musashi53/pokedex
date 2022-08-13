33491138b7d77e36cb37832296797a38c9793330
YuriMendess <contato.yurimendess@gmail.com>
2022-08-12 23:36:41 -0300

    created Flex and Grid components

--- src/components/layout/styles.ts
+++ src/components/layout/styles.ts
@@ -1,8 +1,44 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+interface IGridProps {
+  width?: string;
+  gap?: string;
+}
+
+interface IFlexProps {
+  maxWidth?: string;
+  align?: 'center';
+  justify?: 'space-between' | 'center';
+  direction?: 'row' | 'column';
+  columnGap?: string;
+  margin?: string;
+}
 
 export const Container = styled.div`
   max-width: 1368px;
   margin: 0 1.5rem;
 
   @media screen and (min-width: 1368px) { margin: auto }
+`;
+
+export const Grid = styled.div<IGridProps>`
+  ${(IGridProps) => css`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(${IGridProps.width}, 1fr));
+    gap: ${IGridProps.gap}
+  `}
+`;
+
+export const Flex = styled.div<IFlexProps>`
+  ${(IFlexProps) => css`
+    width: 100%;
+    max-width: ${IFlexProps.maxWidth};
+    display: flex;
+    flex-direction: ${IFlexProps.direction};
+    justify-content: ${IFlexProps.justify};
+    align-items: ${IFlexProps.align};
+    margin: ${IFlexProps.margin};
+    column-gap: ${IFlexProps.columnGap};
+  `}
+  
 `;
