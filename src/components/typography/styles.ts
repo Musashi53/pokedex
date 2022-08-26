1f07e328fe46924dbdc431a1cb3e3f2f192fbf6f
YuriMendess <contato.yurimendess@gmail.com>
2022-08-25 23:40:03 -0300

    create a typography components

---
+++ src/components/typography/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-weight: bold;
+  font-size: 100px;
+`;
+
+export const AplicationTitle = styled.h2`
+  font-weight: bold;
+  font-size: 32px;
+  margin-bottom: 1rem;
+`;
+
+export const PokemonName= styled.h3`
+  font-weight: bold;
+  font-size: 26px;
+  margin-bottom: .5rem;
+`;
+
+export const Description = styled.p`
+  font-weight: regular;
+  font-size: 16px;
+`;
+
+export const PokemonNumber = styled.span`
+  font-weight: bold;
+  font-size: 12px;
+`;
+
+export const PokemonType = styled.span`
+  font-weight: normal;
+  font-size: 12px;
+`;
